d357a5d4a15f051b3ee7b924c4b7d99437e42d9c
djinylh <d.jinylh@gmail.com>
2025-07-07 16:14:52 +0900

    쇼핑몰 제작 첫 부분

---
+++ src/main/java/kr/co/wikibook/gallery/item/ItemController.java
@@ -0,0 +1,13 @@
+package kr.co.wikibook.gallery.item;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ItemController {
+   private final ItemService itemService;
+}
