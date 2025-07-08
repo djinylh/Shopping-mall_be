cefea73374393f951c64da033f68ea957fa8bcbe
djinylh <d.jinylh@gmail.com>
2025-07-08 18:07:35 +0900

    로그인, 로그아웃 xml, Req, Res 제작

--- src/main/java/kr/co/wikibook/gallery/account/AccountMapper.java
+++ src/main/java/kr/co/wikibook/gallery/account/AccountMapper.java
@@ -1,13 +1,14 @@
 package kr.co.wikibook.gallery.account;
 
+import kr.co.wikibook.gallery.account.model.AccountJoinReq;
 import kr.co.wikibook.gallery.account.model.AccountLoginReq;
 import kr.co.wikibook.gallery.account.model.AccountLoginRes;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AccountMapper {
-    AccountLoginRes findByLoginIdAndLoginPw(AccountLoginReq req);
-
+    AccountLoginRes findByLoginId(AccountLoginReq req);
+    int save(AccountJoinReq req);
 
 
 }
--- src/main/java/kr/co/wikibook/gallery/account/AccountService.java
+++ src/main/java/kr/co/wikibook/gallery/account/AccountService.java
@@ -1,7 +1,11 @@
 package kr.co.wikibook.gallery.account;
 
+import kr.co.wikibook.gallery.account.model.AccountJoinReq;
+import kr.co.wikibook.gallery.account.model.AccountLoginReq;
+import kr.co.wikibook.gallery.account.model.AccountLoginRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -9,4 +13,31 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountMapper accountMapper;
+
+    public int join(AccountJoinReq req) {
+        String hashedPw = BCrypt.hashpw(req.getLoginPw(), BCrypt.gensalt());
+        //암호화가 된 비밀번호를 갖는 AccountJoinReq객체를 만들어주세요
+
+        AccountJoinReq changedReq = new AccountJoinReq(req.getName(), req.getLoginId(), hashedPw);
+
+        return accountMapper.save(changedReq);
+    }
+
+    public AccountLoginRes login(AccountLoginReq req) {
+        AccountLoginRes res = accountMapper.findByLoginId(req);
+        // 비밀번호 체크 ( 앞에는 암호화 전 불린 , 뒤에는 암호화 후 불린 )
+        if ( res == null || !BCrypt.checkpw(req.getLoginPw(), res.getLoginPw())) {
+            return null;
+        }
+        return res;
+
+    }
+
+
+
 }
+
+
+
+
+
